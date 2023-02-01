ae5cbe0792444a55794d42d337ae38f7d4ac22ea
JMaycol-Valencia <maycolvalencia50@gmail.com>
2023-02-01 17:06:43 -0400

    iniciando repo - tipos de datos primitivos y no primitivos - typeof y console.log

---
+++ valores.js
@@ -0,0 +1,29 @@
+//VALORES PRIMITIVOS DE DATOS EN JS
+//Numeros
+40
+//Cadenas de texto
+"Maycol Valencia"
+//booleanos
+true 
+false
+//Valores vacios o valores recervados
+//null
+//undefined
+
+
+//VALORES NO PRIMITIVOS
+//Array
+[1,2,3]
+
+//OBJETOS
+{nombre : "maycol"}
+
+//CODIGO EJECUTADO
+//VERIFICAMOS EL TIPO DE DATO
+console.log(typeof(40));
+console.log(typeof("number"));
+console.log(typeof(true));
+console.log(typeof(null));  //CASO CURIOSO DE JS
+console.log(typeof(undefined));
+console.log(typeof([1,2,3]));
+console.log(typeof({nombre : "maycol"}));
---
+++ variables.js
@@ -0,0 +1,20 @@
+//VARIABLES
+var nombre  = "maycol";
+var apellido;       //declaramos variable
+apellido = "valencia";       //Inicializamos la variable
+var elementos = ["juan", "maria", "pedro"];
+var persona = {
+                nombre : "vanesa",
+                apellido : "mendoza"
+            }
+
+
+//EJECUCION
+console.log("INICIAMOS EL PROGRAMA");
+console.log(nombre);
+console.log(apellido);
+console.log(typeof(elementos));
+console.log(elementos);
+console.log(elementos[2])
+console.log(typeof(persona));
+console.log(persona.nombre + " " + persona.apellido);
